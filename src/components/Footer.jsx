05b79ad8c2290fbd1d201826e9f8f4fc6883ef86
Audrey <keozon@gmail.com>
2023-10-13 15:57:26 +0200

    link to my another project

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,12 @@ export default function Footer() {
     <footer>
       <div className='container'>
         <a href='https://github.com/odrinl/dutch-comics'>&copy; Audrey 2023</a>
+        <p>
+          My another project -&nbsp;
+          <a href='https://dutchword.netlify.app/'>
+            Dutch Word Translation app
+          </a>
+        </p>
       </div>
     </footer>
   );

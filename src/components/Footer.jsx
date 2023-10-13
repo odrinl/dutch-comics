b2027ebb1fb3328ff0bc8755700840464a540cdd
Audrey <keozon@gmail.com>
2023-10-13 16:00:29 +0200

    changed text of link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,7 @@ export default function Footer() {
         <a href='https://github.com/odrinl/dutch-comics'>&copy; Audrey 2023</a>
         <p>
           My another project -&nbsp;
-          <a href='https://dutchword.netlify.app/'>
-            Dutch Word Translation app
-          </a>
+          <a href='https://dutchword.netlify.app/'>Dutch Word app</a>
         </p>
       </div>
     </footer>
